a8b605887f36e9b5eae20773b7ede4f72a2fbf6e
Kaloyan Ivanov <kaloyanivanov@hotmail.com>
2025-02-17 16:48:33 +0100

    Redesigned and simplified projects page

--- source/js/project_page/poject_item.js
+++ source/js/project_page/poject_item.js
@@ -1,77 +1,36 @@
 $(document).ready(function () {
 
-    // Preload the GIF
-    $("[data-demo-src]").each(function () {
-        let gifSrc = $(this).data("demo-src");
+    $('.item-bg-projects').each(function () {
+
+        const $this = $(this);
+
+        $this.data('originalBoxShadow', $this.css('box-shadow'));
+
+        // Set initial bg image
+        let originalBgSrc = $this.data('thumbnail-src'); // Get src from data attribute
+        if (originalBgSrc != 'none') {
+            $this.css('background-image', 'url("' + originalBgSrc + '")');
+            adjustHeightToFitImage($this, originalBgSrc); // Adjust height so that it matches the image' width
+        }
+
+        // Preload the GIF (for better experienve)
+        let gifSrc = $this.data("demo-src");
         let img = new Image();
         img.src = gifSrc;
+
     });
 
-    $('.item-bg-projects').each(function () {
-        $(this).data('originalWidth', $(this).width());
-        $(this).data('originalHeight', $(this).height());
-        $(this).data('originalBg', $(this).css('background-image'));
-        $(this).data('originalBoxShadow', $(this).css('box-shadow'));
-
-        $(this).data("href", $(this).attr("href")).removeAttr("href");
-    });
-
-    const $gridContainer = $('#main-projects-grid');
-
-    let $currentOpenItem = null;
-
-    // TODO: Fix weird interactions
-
-    $('.item-bg-projects').mousedown(
+    $('.item-bg-projects').mouseenter(
         function () {
 
             const $this = $(this);
 
-            if ($currentOpenItem && $currentOpenItem.is($this)) {
-                return;
-            }
-            $currentOpenItem = $this;
+            // Switch to 'on-hover' box shadow
+            $this.css('box-shadow', '10px 5px 20px 0px #765724'); // '-10px 5px 20px 0px #765724'
 
-            const width = $this.outerWidth();
-
-            const isLeft = $this.hasClass("left-item-projects")
-            const offsetAmount = isLeft ? -width : width;
-
-            $this.animate(
-                {
-                    width: $gridContainer.width(),
-                    left: `${isLeft
-                        ? offsetAmount - ($gridContainer.width() * (parseFloat($gridContainer.css('column-gap')) / 100)/* Get that extra gap */)
-                        : 0}px`
-                },
-                'slow',
-                function () {
-
-                    $this.css('box-shadow', isLeft ? '-10px 5px 20px 0px #765724' : '10px 5px 20px 0px #765724');
-                    $this.css('z-index', 3);
-
-                    // Enlarge height
-                    let newBgSrc = $this.data('demo-src');
-
-                    // let background = $this.css('background-image');
-
-                    if (newBgSrc != 'none') {
-
-                        // background = background.replace('url("', '').replace('")', '');
-
-                        $this.css('background-image', 'url("' + newBgSrc + '")');
-                        adjustHeightToFitImage($this, newBgSrc);
-
-                    }
-
-
-                    $(this).attr("href", $(this).data("href"));
-
-                });
-
-            $this.siblings().each(function () {
-                $(this).stop(true).animate({ left: `${-offsetAmount + (isLeft ? 1 : -1) * ($gridContainer.width() * (parseFloat($gridContainer.css('column-gap')) / 100)/* Get that extra gap */)}px` }, 'slow');
-            });
+            // Switch to 'on-hover' background image (gif)
+            let newBgSrc = $this.data('demo-src');
+            $this.css('background-image', 'url("' + newBgSrc + '")');
 
         }
     );
@@ -81,32 +40,12 @@ $(document).ready(function () {
 
             const $this = $(this);
 
-            if ($currentOpenItem && !$currentOpenItem.is($this)) {
-                return;
-            }
-            $currentOpenItem = null;
+            // Switch back to original box shadow
+            $this.css('box-shadow', $this.data('originalBoxShadow'));
 
-            $(this).removeAttr("href");
-
-            $this.stop(true).animate(
-                {
-                    width: $this.data('originalWidth'),
-                    height: $this.data('originalHeight'),
-                    left: '0px'
-                },
-                'fast',
-                function () {
-                    $this.css('background-image', $this.data('originalBg'));
-                    $this.css('box-shadow', $this.data('originalBoxShadow'));
-                    $this.css('z-index', 2);
-
-                    $previousItemSelected = null;
-                });
-
-            $this.siblings().each(function () {
-                $(this).stop(true).animate({ left: '0px' }, function () {
-                });
-            });
+            // Switch back to original background image (thumbnail)
+            let originalBgSrc = $this.data('thumbnail-src');
+            $this.css('background-image', 'url("' + originalBgSrc + '")');
 
         }
     );
@@ -122,12 +61,20 @@ $(document).ready(function () {
             const width = $element.width();
             const height = width / aspectRatio; // Calculate height based on width and aspect ratio
 
-            $element.animate(
-                {
-                    height: height
-                },
-                'slow');
+            $element.height(height);
         };
     }
 
+    $(window).resize(function () {
+        $('.item-bg-projects').each(function () {
+
+            const $this = $(this);
+
+            let originalBgSrc = $this.data('thumbnail-src'); // Get src from data attribute
+            if (originalBgSrc != 'none') {
+                adjustHeightToFitImage($this, originalBgSrc); // Adjust height so that it matches the image' width
+            }
+
+        });
+    });
 });
